7629e2d22e7a447cfa9279a2303d79ffb36e04ff
vaidas <valikonis.vaidas@gmail.com>
2024-09-10 12:26:59 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {useState} from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+          <img src={"./src/assets/dashb_icon.svg"} />
+      </div>
+    </>
+  )
+}
+
+export default App
